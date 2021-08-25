a194dd9a7f4528cf93cf28f93ac32b2d1977de96
RoiSukenik <roisukenik@gmail.com>
2021-08-25 11:24:38 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import 'dotenv/config';
 import App from './app';
 import { AddressController, DeliveryController, TimeSlotsController } from './controllers';
 
-const app = new App([new AddressController(), new DeliveryController(), new TimeSlotsController()], 5000);
+const app = new App([
+                new AddressController(), 
+                new DeliveryController(), 
+                new TimeSlotsController()], 5000);
 
 app.listen();
